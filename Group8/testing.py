5ce92fde644cb6149a6c54d0556572b07e8392be
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-26 15:47:21 -0600

    get length of each step

--- Group8/testing.py
+++ Group8/testing.py
@@ -16,9 +16,9 @@ def get_k_pos(n_s, n_t, r_s, r_h):
 
 # the difference of the nodes’ position along their individual branches 
 # given in percentage of the complete branch length
-def get_k_perc(n_s, n_t, end_s, end_h):
-
-	return n_s[0]+n_t[0]
+def get_k_perc(length_n_s, length_n_t, length_b_a, length_b_h):
+	return 0
+	#return length_n_t/length_b_h - length_n_s/length_b_a
 
 # the difference of the nodes’ degree centrality
 def get_k_deg(n_s, n_t, r_s, r_h):
@@ -33,17 +33,17 @@ def get_k_area(n_s, n_t, r_s, r_h):
 	return 0
 
 # cost K(Nsh, Nta) of assigning two node
-def get_k(b_a, b_h, i, j):
+def get_k(b_a, b_h, i, j, length_b_a, length_b_h):
 	α1 = 10.0
 	α2 = 2.0
 	α3 = 0.2
 	α4 = 1.0
 	α5 = 0.5
-	value = α1 * get_k_pos (b_a[i], b_h[j], b_a[ 0], b_h[ 0]) + \
-			α2 * get_k_perc(b_a[i], b_h[j], b_a[-1], b_h[-1]) + \
-			α3 * get_k_deg (b_a[i], b_h[j], b_a[ 0], b_h[ 0]) + \
-			α4 * get_k_bet (b_a[i], b_h[j], b_a[ 0], b_h[ 0]) + \
-			α5 * get_k_area(b_a[i], b_h[j], b_a[ 0], b_h[ 0])
+	value = α1 * get_k_pos (b_a[i], b_h[j],    b_a[ 0],    b_h[ 0]) + \
+			α2 * get_k_perc(b_a[i], b_h[j], length_b_a, length_b_h) + \
+			α3 * get_k_deg (b_a[i], b_h[j],    b_a[ 0],    b_h[ 0]) + \
+			α4 * get_k_bet (b_a[i], b_h[j],    b_a[ 0],    b_h[ 0]) + \
+			α5 * get_k_area(b_a[i], b_h[j],    b_a[ 0],    b_h[ 0])
 	return value
 
 # get the cost of mapping
@@ -57,7 +57,6 @@ def get_mapping_cost(b_a, b_h):
 		d_y_a = b_a[i][1] - b_a[i-1][1]
 		d_z_a = b_a[i][2] - b_a[i-1][2]
 		length_b_a += sqrt(d_x_a**2 + d_y_a**2 + d_z_a**2)
-	print (length_b_a)
 
 	# get the length of branch h
 	for i in range(1,len(b_h)):
@@ -65,12 +64,23 @@ def get_mapping_cost(b_a, b_h):
 		d_y_h = b_h[i][1] - b_h[i-1][1]
 		d_z_h = b_h[i][2] - b_h[i-1][2]
 		length_b_h += sqrt(d_x_h**2 + d_y_h**2 + d_z_h**2)
-	print (length_b_h)
 
+	length_n_s = 0
 	cost_map = [[0 for x in range(len(b_h)-1)] for x in range(len(b_a)-1)] 
 	for i in range(len(cost_map)):
+		length_n_t = 0
+		d_x_a = b_a[i+1][0] - b_a[i][0]
+		d_y_a = b_a[i+1][1] - b_a[i][1]
+		d_z_a = b_a[i+1][2] - b_a[i][2]
+		length_n_s += sqrt(d_x_a**2 + d_y_a**2 + d_z_a**2)
+		
 		for j in range(len(cost_map[0])):
-			cost_map[i][j] = get_k(b_a, b_h, i, j)
+			d_x_h = b_h[j+1][0] - b_h[j][0]
+			d_y_h = b_h[j+1][1] - b_h[j][1]
+			d_z_h = b_h[j+1][2] - b_h[j][2]
+			length_n_t += sqrt(d_x_h**2 + d_y_h**2 + d_z_h**2)
+
+			cost_map[i][j] = get_k(b_a, b_h, i, j, length_b_a, length_b_h)
 	return cost_map
 
 

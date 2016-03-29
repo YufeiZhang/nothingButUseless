94e9d42ea6df0f973742190a5d263b016ce2a85b
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-28 23:47:07 -0600

    there cannot be only 3 branches.

--- Group8/read_skeleton.py
+++ Group8/read_skeleton.py
@@ -4,3 +4,23 @@ This file is to read all branches of both target and source skeleton
 This should be using motion-builder
 '''
 
+file_name = "low.txt"
+#file_name = "high.txt"
+
+txt = open(file_name).read().split(",")
+#print(txt)
+#print(len(txt))
+
+branches = []
+branch   = []
+for i in range(len(txt)):
+	branch.append(txt[i][7:])
+	if "End" in txt[i]:
+		branches.append(branch)
+		branch = []
+		
+print(branches)
+
+
+
+

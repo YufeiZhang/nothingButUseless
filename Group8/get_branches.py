0293ea7ec41fbd5b8fcb907308bdda95224fb838
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-30 13:30:15 -0600

    I think this will work on motion builder. Lets see

--- Group8/get_branches.py
+++ Group8/get_branches.py
@@ -7,57 +7,65 @@ This should be using motion-builder
 '''
 
 def get_banch(parents, children, index, branches):
-	#print(index)
+	print(index)
 	#marker = FBFindModelByLabelName('Marker01')
 	#m_pos = FBVector3d() marker.GetVector(m_pos,FBModelTransformationType.kModelTranslation)
-	parents.append(children[0])
+	parents.append(children.Name)
 
 	# if there is no children, append this branch to branches
-	if len(children) == 1:
+	if len(children.Children) == 1:
 		branches.append(parents)
-		#print()
+		print()
 
 	# if there is a children, then go to the child
-	elif len(children) == 2:
-		parents = get_banch(parents, children[1], index+1, branches)
+	elif len(children.Children) == 2:
+		parents = get_banch(parents, children.Children[0], index+1, branches)
 
 	# if there are several leaves, then search each leaf
 	else:
-		for i in range(1,len(children)):
+		for i in range(len(children.Children)):
 			#print(parents[:index+1])
 			#print(children[i])
 			new = []
-			new = get_banch(parents[:index+1], children[i], index+1, branches)
+			new = get_banch(parents[:index+1], children.Children[i], index+1, branches)
 	#target_rot = FBVector3d(0,90,0)
 	return parents
 
 
-def get_branches(skeleton):
-	#root = FBFindModelByLabelName('???')
+# all I need is just a root node
+# parentNode = node.Parent
+# childCount = len(node.Children)
+# firstChild = node.Children[0]
+def get_branches(root):
 	branches = []
-	if len(skeleton) > 1:
-		for i in range(1,len(skeleton)): # this is to stop the loop
+	print(root)
+	print(root.Name)
+
+	if len(root.Children) > 1:
+		# you need to check len(root.Children)
+		for i in range(root.Children): # this is to stop the loop
 			branch = []
-			branch.append(skeleton[0])
-			#branch.append(root)
+			branch.append(root.Name) # skeleton[0] -> root
 
 			# initialize the node and get its children
 			parents = branch[:len(branch)]
-			children = skeleton[i]
-			#parentNode = node.Parent
-			#childCount = len(node.Children)
-			#firstChild = node.Children[0]
+			children = root.Children[i]
 			
 			# start the loop to find all leaves
+			# the initial index may be wrong, you'd better check it.
 			branch = get_banch(parents, children, 1, branches)
 
 			#print()
 	#print("\n\n\n\n")
+
 	return branches
 
 
 if __name__ == "__main__":
-    branches = get_branches(skeleton)
+	# Chose the node that has the highest betweeness
+	root = FBFindModelByLabelName('JOINT Spine1') 
+	#branches = []
+    branches = get_branches(root)
     print(len(branches)) # this tells you how many branches we have
     print(branches)      # this shows all branches in a list
 

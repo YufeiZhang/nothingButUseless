c5f3443ba18793beb1b208654f1add886cb7ec9d
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-29 23:06:53 -0600

    change name of a file and add some comments

--- Group8/get_leaves.py
+++ Group8/get_leaves.py
@@ -35,27 +35,36 @@ skeleton = \
 #print(skeleton)
 # I should get 7 branches
 
+
+'''
+This file need a skeleton in list
+Maybe I need anothre file to do read a .bvh file 
+and get the output as a python list
+'''
+
 def get_banch(parents, children, index, branches):
 	#print(index)
+	parents.append(children[0])
+
+	# if there is no children, append this branch to branches
 	if len(children) == 1:
-		parents.append(children[0])
-		#print(parents)
 		branches.append(parents)
 		#print()
 
+	# if there is a children, then go to the child
 	elif len(children) == 2:
-		parents.append(children[0])
 		parents = get_banch(parents, children[1], index+1, branches)
 
+	# if there are several leaves, then search each leaf
 	else:
-		parents.append(children[0])
 		for i in range(1,len(children)):
 			#print(parents[:index+1])
 			#print(children[i])
 			new = []
 			new = get_banch(parents[:index+1], children[i], index+1, branches)
-			
+
 	return parents
+
 
 def get_branches(skeleton):
 	branches = []
@@ -64,25 +73,22 @@ def get_branches(skeleton):
 			branch = []
 			branch.append(skeleton[0])
 
+			# initialize the node and get its children
 			parents = branch[:len(branch)]
 			children = skeleton[i]
 			
-			# ---------------------- add code here ----------------------
-
+			# start the loop to find all leaves
 			branch = get_banch(parents, children, 1, branches)
-			# ---------------------- add code  end ----------------------
+
 			#print()
-	#print()
-	#print()
-	#print()
-	#print()
+	#print("\n\n\n\n")
 	return branches
 
 
 if __name__ == "__main__":
     branches = get_branches(skeleton)
-    print(len(branches))
-    print(branches)
+    print(len(branches)) # this tells you how many branches we have
+    print(branches)      # this shows all branches in a list
 
 
 

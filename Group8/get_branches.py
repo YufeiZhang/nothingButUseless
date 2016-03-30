24ee61ecf5767d10b2fb92acc5d0217ffbfb3834
philip <yufeiGitHub@gmail.com>
2016-03-30 16:59:15 -0600

    Update get_branches.py

--- Group8/get_branches.py
+++ Group8/get_branches.py
@@ -1,3 +1,6 @@
+ny branches we have
+#print(branches)     # this shows all branches in a list
+#print(branches_posi)
 from pyfbsdk import *
 import math
 
@@ -44,14 +47,12 @@ def get_branches(root):
             # start the loop to find all leaves
             # the initial index may be wrong, you'd better check it.
             branch = get_banch(parents, children, 1, branches)
-
-            #print()
-    #print("\n\n\n\n")
-
+            
     return branches
 
 
-def get_branches_posi(branches):
+def get_branches_posi(branches, file_name):
+    out = open(file_name,"w")
     branches_posi = []
     node = FBVector3d()
     for b in branches:
@@ -60,14 +61,24 @@ def get_branches_posi(branches):
             n = FBFindModelByLabelName(name)
             n.GetVector(node, FBModelTransformationType.kModelTranslation)
             bran_posi.append(node)
+            
+            out.write(repr(node[0]) + " ")
+            out.write(repr(node[1]) + " ")
+            out.write(repr(node[2]) + '\n')    
+        out.write('------------------------------------\n')
         branches_posi.append(bran_posi)
+    out.close()
     return branches_posi
 
 # Chose the node that has the highest betweeness
+#root = FBFindModelByLabelName('PMD_Kristoff__summer_')
 root = FBFindModelByLabelName('Bip01') 
 branches = get_branches(root)
-branches_posi = get_branches_posi(branches)
-
-#print(len(branches)) # this tells you how many branches we have
+branches_posi = get_branches_posi(branches, "1.txt")
+print(len(branches)) # this tells you how many branches we have
 #print(branches)     # this shows all branches in a list
-#print(branches_posi)
+
+root2 = FBFindModelByLabelName('PMD_Kristoff__summer_') 
+branches2 = get_branches(root2)
+branches_posi2 = get_branches_posi(branches2, "2.txt")
+print(len(branches2))

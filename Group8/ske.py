52586364f4b5b636747f549a2a53d6a82f9133f7
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-29 20:58:06 -0600

    It can read all nodes, but the number of leaves is not right

--- Group8/ske.py
+++ Group8/ske.py
@@ -35,20 +35,53 @@ skeleton = \
 #print(skeleton)
 # I should get 7 branches
 
-branches = []
 
-if len(skeleton) > 1:
-	for i in range(1, len(skeleton)):
-		barnch   = []
-		barnch.append(skeleton[0]) # ["Hip"]
-		current = skeleton[i]
-		while len(current) != 1:
-			barnch.append(current[0])
-			current = current[1]
-		barnch.append(current[0])
-		print(barnch)
-		
+
+
+def get_banch(parents, children, branches):
+	if len(children) == 1:
+		parents.append(children[0])
+		branches.append(parents)
+
+	elif len(children) == 2:
+		parents.append(children[0])
+		parents = get_banch(parents, children[1], branches)
+
+	else:
+		parents.append(children[0])
+		print("------------------")
+		for i in range(1,len(children)):
+			parents = get_banch(parents, children[i], branches)
+	return parents
+
+def get_branches(skeleton):
+	branches = []
+	if len(skeleton) > 1:
+		for i in range(1,len(skeleton)): # this is to stop the loop
+			branch = []
+			branch.append(skeleton[0])
+
+			parents = branch[:len(branch)]
+			children = skeleton[i]
 			
+			# ---------------------- add code here ----------------------
+
+			branch = get_banch(parents, children, branches)
+
+			# ---------------------- add code end ----------------------
+			
+			print(branch)
+			print()
+
+
+	return branches
+	
+
+
+
+
+if __name__ == "__main__":
+    get_branches(skeleton)
 
 
 

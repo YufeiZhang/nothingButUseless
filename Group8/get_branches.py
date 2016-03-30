cb80fa2465e1fdf89d0842e47b438b88f8e2fd5d
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-30 12:32:41 -0600

    update what maybe used

--- Group8/get_branches.py
+++ Group8/get_branches.py
@@ -8,6 +8,8 @@ This should be using motion-builder
 
 def get_banch(parents, children, index, branches):
 	#print(index)
+	#marker = FBFindModelByLabelName('Marker01')
+	#m_pos = FBVector3d() marker.GetVector(m_pos,FBModelTransformationType.kModelTranslation)
 	parents.append(children[0])
 
 	# if there is no children, append this branch to branches
@@ -26,21 +28,25 @@ def get_banch(parents, children, index, branches):
 			#print(children[i])
 			new = []
 			new = get_banch(parents[:index+1], children[i], index+1, branches)
-
+	#target_rot = FBVector3d(0,90,0)
 	return parents
 
 
 def get_branches(skeleton):
+	#root = FBFindModelByLabelName('???')
 	branches = []
 	if len(skeleton) > 1:
-		# len(skeleton) maybe can be changed as number of children
 		for i in range(1,len(skeleton)): # this is to stop the loop
 			branch = []
 			branch.append(skeleton[0])
+			#branch.append(root)
 
 			# initialize the node and get its children
 			parents = branch[:len(branch)]
 			children = skeleton[i]
+			#parentNode = node.Parent
+			#childCount = len(node.Children)
+			#firstChild = node.Children[0]
 			
 			# start the loop to find all leaves
 			branch = get_banch(parents, children, 1, branches)
@@ -51,13 +57,11 @@ def get_branches(skeleton):
 
 
 if __name__ == "__main__":
-	# on motion builder, maybe I just need to get chain_base instead of skeleton
-	'''
-	The output should be a list of list to store all branckes
-	'''
     branches = get_branches(skeleton)
     print(len(branches)) # this tells you how many branches we have
     print(branches)      # this shows all branches in a list
+
+
 
 
 
